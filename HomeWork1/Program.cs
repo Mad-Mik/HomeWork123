cad52fdd65f0ce50d3bfbd3bf6d253ece8697361
mikhail.madoyan <mikhail.madoyan@gmail.com>
2022-12-29 23:50:56 +0400

    Добавьте файлы проекта.

---
+++ HomeWork1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork1
+{
+    //mijin tvabanakan arjeq
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstValue, secondValue;
+
+            Console.WriteLine("number 1");
+
+            firstValue = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("number 2");
+
+            secondValue = double.Parse(Console.ReadLine());
+
+            double result = (firstValue + secondValue) / 2;
+
+            Console.WriteLine("middleValue = " + result);
+        }
+    }
+}
